2ced98c0d99cc8122472da6a0404e4fc5c194460
abelcarriizo <abelcarriizo@outlook.com>
2024-11-22 16:41:14 -0300

    Nuevo. Se añadio un paquete de inicializacion

---
+++ src/resources/__init__.py
@@ -0,0 +1,6 @@
+from .user import User as UserResource
+from .user import Users as UsersResource
+from .poem import Poem as PoemResource
+from .poem import Poems as PoemsResource
+from .rating import Rating as RatingResource
+from .rating import Ratings as RatingsResource
