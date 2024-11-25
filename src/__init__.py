ec2f96c4bfbcc752c858453f364b5ab87e444d38
abelcarriizo <abelcarriizo@outlook.com>
2024-11-25 17:16:47 -0300

    Actualizado. Se añadio la configuracion de la base de datos, ademas de las rutas de los recursos.

--- src/__init__.py
+++ src/__init__.py
@@ -1,26 +1,39 @@
+import os
 from flask import Flask
 from dotenv import load_dotenv
 from flask_restful import Api
-import src.resources as resources
+from flask_sqlalchemy import SQLAlchemy
 
 api = Api() # Inicializa API de Flask Restful
+db = SQLAlchemy() # Inicializa SQLAlchemy
 
 # Método que inicializará todos los módulos y devolverá la aplicación
 def create_app():
-  app = Flask(__name__) # Inicializa Flask
+  app = Flask(__name__) # Inicializa Flask  
 
   load_dotenv() # Carga las variables de entorno
+
+  #Configuracion de Base de Datos
+  app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+  app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+mysqlconnector://{os.getenv('DATABASE_USER')}:{os.getenv('DATABASE_PASSWORD')}@localhost/{os.getenv('DATABASE_NAME')}"
+
+  db.init_app(app)
+
+  import src.resources as resources
 
   # Carga a la API los recursos
   #Todos los recursos
   api.add_resource(resources.UsersResource, '/users')
+  api.add_resource(resources.AdminsResource, '/admins')
   api.add_resource(resources.PoemsResource, '/poems')
-  api.add_resource(resources.RatingsResource, '/ratings')
+  # */ratings* se utilizara para crear ratings. */ratings/poem* se utilizara para obtener todos los ratings de un poema.
+  api.add_resource(resources.RatingsResource, '/ratings', '/ratings/poem/<int:poem_id>')
   
   #Recursos por ID
-  api.add_resource(resources.UserResource, '/user/<id>')
-  api.add_resource(resources.PoemResource, '/poem/<id>')
-  api.add_resource(resources.RatingResource, '/rating/<id>')
+  api.add_resource(resources.UserResource, '/user/<int:id>')
+  api.add_resource(resources.AdminResource, '/admin/<int:admin_id>')
+  api.add_resource(resources.PoemResource, '/poem/<int:id>')
+  api.add_resource(resources.RatingResource, '/rating/<int:id>')
 
   api.init_app(app) # Carga la aplicacion en la API de Flask Restful
   
