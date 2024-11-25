8585e6e54f18c60940163ee2589971d207c8b00d
abelcarriizo <abelcarriizo@outlook.com>
2024-11-25 16:56:53 -0300

    Actualizado. Se añadio la configuracion para la creacion de la base de datos

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
-from src import create_app
+from src import create_app, db
 
 app = create_app()
+app.app_context().push()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("FLASK_PORT"))
+  db.create_all()
+  app.run(debug=True, port=os.getenv("FLASK_PORT"))
