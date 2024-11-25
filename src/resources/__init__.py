260b9db0944b228c5f643496239d7dbec1f65a8d
abelcarriizo <abelcarriizo@outlook.com>
2024-11-25 17:13:37 -0300

    Actualizado. Se añadieron nuevas entidades en el paquete de inicializacion

--- src/resources/__init__.py
+++ src/resources/__init__.py
@@ -1,5 +1,7 @@
 from .user import User as UserResource
 from .user import Users as UsersResource
+from .admin import Admin as AdminResource
+from .admin import Admins as AdminsResource 
 from .poem import Poem as PoemResource
 from .poem import Poems as PoemsResource
 from .rating import Rating as RatingResource
