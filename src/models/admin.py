1514877242f4c276ccc8d2efac6a4c4375ec2126
abelcarriizo <abelcarriizo@outlook.com>
2024-12-01 17:04:19 -0300

    Actualizacion. Se añadio encriptacion al almacenamiento de contraseñas para mayor seguridad

--- src/models/admin.py
+++ src/models/admin.py
@@ -1,4 +1,5 @@
 from ..import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class Admin(db.Model):
   __tablename__ = 'Admins'
@@ -8,7 +9,22 @@ class Admin(db.Model):
   lastname = db.Column(db.String(15), nullable=False)
   username = db.Column(db.String(40), nullable=False)
   email = db.Column(db.String(40), nullable=False)
-  password = db.Column(db.String(40), nullable=False)
+  password = db.Column(db.String(255), nullable=False)
+
+  #Getter de la contraseña plana no permite leerla
+  @property
+  def plain_password(self):
+    raise AttributeError('Password cant be read')
+  
+  #Setter de la contraseña toma un valor en texto plano
+  # calcula el hash y lo guarda en el atributo password
+  @plain_password.setter
+  def plain_password(self, password):
+    self.password = generate_password_hash(password)
+    
+  #Método que compara una contraseña en texto plano con el hash guardado en la db
+  def validate_pass(self,password):
+    return check_password_hash(self.password, password)
 
   def __repr__(self) -> str:
     return f'Usuario: {self.username}'
@@ -32,4 +48,4 @@ class Admin(db.Model):
     username = user_json.get("username")
     email = user_json.get("email")
     password = user_json.get("password")
-    return Admin(id=id, firstname=firstname, lastname=lastname, username=username, email=email, password=password)
+    return Admin(id=id, firstname=firstname, lastname=lastname, username=username, email=email, plain_password=password)
--- src/models/user.py
+++ src/models/user.py
@@ -1,4 +1,5 @@
 from ..import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
   __tablename__ = 'Users'
@@ -10,10 +11,25 @@ class User(db.Model):
   gender = db.Column(db.String(10), nullable=False)
   description = db.Column(db.String(100), nullable=False)
   email = db.Column(db.String(40), nullable=False)
-  password = db.Column(db.String(40), nullable=False)
+  password = db.Column(db.String(255), nullable=False)
 
   poems = db.relationship('Poem', back_populates='author', cascade='all, delete-orphan')
   ratings = db.relationship('Rating', back_populates='author', cascade='all, delete-orphan')
+  
+  #Getter de la contraseña plana no permite leerla
+  @property
+  def plain_password(self):
+    raise AttributeError('Password cant be read')
+  
+  #Setter de la contraseña toma un valor en texto plano
+  # calcula el hash y lo guarda en el atributo password
+  @plain_password.setter
+  def plain_password(self, password):
+    self.password = generate_password_hash(password)
+    
+  #Método que compara una contraseña en texto plano con el hash guardado en la db
+  def validate_pass(self,password):
+    return check_password_hash(self.password, password)
 
   def __repr__(self) -> str:
     return f'Usuario: {self.username}'
@@ -41,4 +57,4 @@ class User(db.Model):
     description = user_json.get("description")
     email = user_json.get("email")
     password = user_json.get("password")
-    return User(id=id, firstname=firstname, lastname=lastname, username=username, gender=gender, description=description, email=email, password=password)
+    return User(id=id, firstname=firstname, lastname=lastname, username=username, gender=gender, description=description, email=email, plain_password=password)
