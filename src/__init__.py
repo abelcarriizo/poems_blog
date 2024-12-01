a623de48b11b4c8d11dba0e9b4a3f9b0d2c3eb67
abelcarriizo <abelcarriizo@outlook.com>
2024-12-01 17:06:12 -0300

    Actualizacion. Se añadio la configuracion para el soporte de JWT

--- src/__init__.py
+++ src/__init__.py
@@ -3,9 +3,11 @@ from flask import Flask
 from dotenv import load_dotenv
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
 
 api = Api() # Inicializa API de Flask Restful
 db = SQLAlchemy() # Inicializa SQLAlchemy
+jwt = JWTManager() # Inicializa Flask JWT
 
 # Método que inicializará todos los módulos y devolverá la aplicación
 def create_app():
@@ -35,5 +37,14 @@ def create_app():
   api.add_resource(resources.RatingResource, '/rating/<int:id>')
 
   api.init_app(app) # Carga la aplicacion en la API de Flask Restful
+    
+  app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
+  app.config["JWT_ACCESS_TOKEN_EXPIRES"] = int(os.getenv("JWT_ACCESS_TOKEN_EXPIRES"))
+  
+  jwt.init_app(app)
+  
+  from src.auth.routes import auth
+
+  app.register_blueprint(auth) #Import blueprint for routes.auth
   
   return app
